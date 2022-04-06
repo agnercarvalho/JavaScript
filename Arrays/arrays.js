752d7eff39cf911fe82932a5a363c051406ea058
Agner C <agnerf@gmail.com>
2022-04-06 19:20:43 -0300

    Arrays
    Revisão sobre

---
+++ Arrays/arrays.js
@@ -0,0 +1,23 @@
+const names = ["ag", "nd", "r"];
+
+console.log(names);
+
+names[1] = "ne";
+
+console.log(names);
+
+// Reatribuir valores em um array, mesmo sendo CONST, não retorna erro (funciona com objetos tbm)
+
+names.push("!"); // adiciona um valor no final do array, mesmo sendo const não dá erro
+
+// Se tentar declarar outro array/obj com mesmo nome vai dar erro
+// names = ["faca", "queijo"];
+
+const newNames = names;
+
+newNames.push("Novo item");
+
+console.log(names);
+console.log(newNames);
+
+// Mesmo quando você copia arrays e adiciona algum valor no que foi criado, o novo valor é 
