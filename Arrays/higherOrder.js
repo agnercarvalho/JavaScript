b1d3ac19b3fc6ab7738d01942d4eb268c3f76b4e
Agner C <agnerf@gmail.com>
2022-04-06 21:16:00 -0300

    Update higherOrder.js

--- Arrays/higherOrder.js
+++ Arrays/higherOrder.js
@@ -30,3 +30,26 @@ repeater(sayHello);
 // As funções de CALLBACK são as funções inseridas dentro de outras, ou funções que esperam outras coisas acontecerem. Por exemplo:
 
 // button.addEventListener("click", function(){...}); a função só será executada depois do click, ela espera alguma coisa acontecer para ela ser chamada 
+
+// MAP
+
+// Map é uma função de ordem maior que permite retornar valores manipulados pela função que é usada como parâmetro. Por Exemplo:
+
+const newCoisas = coisas.map(function(coisa){
+    return coisa.toUpperCase();  //você pode chamar novas funções dentro desse escopo e sobrescrever o array
+
+    // Atenção: se você não retornar nada dentro da função, ela retornará um array undefined
+});
+
+console.log(newCoisas);
+
+// O MAP permite que você desvincule arrays, tornando possível adicionar e remover valores. EX:
+
+const copiaCoisas = coisas.map(function(coisa){
+    return coisa;
+});
+
+copiaCoisas.push("Nova coisa!");
+
+console.log(coisas);
+console.log(copiaCoisas);
